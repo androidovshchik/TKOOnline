0f67de366fbadf4736caed638a7bdd3e6105a96a
vladislav.k <vladislav.k@appstockus.com>
2019-09-11 21:05:15 +0300

    init

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019. Vlad Kalyuzhnyu <vladkalyuzhnyu@gmail.com>
+ */
+@file:Suppress("unused")
+
+object Versions {
+    const val kotlin = "1.3.50"
+    const val kodein = "6.3.3"
+    const val anko = "0.10.8"
+    const val room = "2.2.0-beta01"
+    const val lifecycle = "2.1.0"
+}
