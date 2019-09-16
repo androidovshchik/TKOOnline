8e5984ab881e1e1603e7308d4974ba4feb96ce8f
vladislav.k <vladislav.k@appstockus.com>
2019-09-16 09:57:07 +0300

    init

--- app/src/main/java/ru/iqsolution/tkoonline/screens/login/BarcodeFragment.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/screens/login/BarcodeFragment.kt
@@ -1,22 +1,23 @@
 package ru.iqsolution.tkoonline.screens.login
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
-import androidx.core.util.forEach
 import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.Detector
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
-import org.jetbrains.anko.collections.forEach
+import org.jetbrains.anko.UI
 import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.surfaceView
 import ru.iqsolution.tkoonline.DANGER_PERMISSIONS
-import ru.iqsolution.tkoonline.R
 import ru.iqsolution.tkoonline.extensions.areGranted
 import ru.iqsolution.tkoonline.screens.BaseFragment
 import timber.log.Timber
 import java.io.IOException
 
+@Suppress("DEPRECATION")
 class BarcodeFragment : BaseFragment() {
 
     private lateinit var barcodeDetector: BarcodeDetector
@@ -26,13 +27,30 @@ class BarcodeFragment : BaseFragment() {
     private lateinit var cameraView: SurfaceView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val width = appContext?.resources?.getDimensionPixelSize(R.dimen.barcode_width) ?: 1600
+        //val width = appContext?.resources?.getDimensionPixelSize(R.dimen.barcode_width) ?: 1024
         barcodeDetector = BarcodeDetector.Builder(appContext)
             .setBarcodeFormats(Barcode.ALL_FORMATS)
             .build()
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+
+            override fun receiveDetections(detections: Detector.Detections<Barcode>) {
+                detections.apply {
+                    if (detectorIsOperational()) {
+                        detectedItems.forEach { _, barcode: Barcode ->
+                            Timber.d(barcode.rawValue)
+                        }
+                    }
+                }
+            }
+
+            override fun release() {}
+        })
         cameraSource = CameraSource.Builder(appContext, barcodeDetector)
             .setAutoFocusEnabled(true)
             .build()
+        UI {
+
+        }.view
         cameraView = SurfaceView(appContext).apply {
             layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
             holder.addCallback(object : SurfaceHolder.Callback {
@@ -56,30 +74,60 @@ class BarcodeFragment : BaseFragment() {
                 }
             })
         }
-        return cameraView
+        return UI {
+            cameraView = surfaceView {
+                layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
+                holder.addCallback(object : SurfaceHolder.Callback {
+
+                    @SuppressLint("MissingPermission")
+                    override fun surfaceCreated(holder: SurfaceHolder) {
+                        startPreview()
+                    }
+
+                    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
+
+                    override fun surfaceDestroyed(holder: SurfaceHolder) {
+                        cameraSource.stop()
+                    }
+                })
+            }
+        }.view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+        if (appContext?.areGranted(*DANGER_PERMISSIONS) != true) {
+            requestPermissions(DANGER_PERMISSIONS, REQUEST_PERMISSIONS)
+        }
+    }
 
-            @Suppress("DEPRECATION")
-            override fun receiveDetections(detections: Detector.Detections<Barcode>) {
-                detections.apply {
-                    if (detectorIsOperational()) {
-                        detectedItems.forEach { _, barcode: Barcode ->
-                            Timber.d(barcode.rawValue)
-                        }
-                    }
-                }
+    private fun startPreview() {
+        try {
+            if (appContext?.areGranted(Manifest.permission.CAMERA) == true) {
+                cameraSource.start(cameraView.holder)
+                cameraSource.previewSize
             }
+        } catch (e: IOException) {
+            Timber.e(e)
+        }
+    }
 
-            override fun release() {}
-        })
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSIONS) {
+            if (appContext?.areGranted(Manifest.permission.CAMERA) == true) {
+                startPreview()
+            }
+        }
     }
 
     override fun onDestroyView() {
         barcodeDetector.release()
         cameraSource.release()
         super.onDestroyView()
+    }
+
+    companion object {
+
+        private const val REQUEST_PERMISSIONS = 100
     }
 }
