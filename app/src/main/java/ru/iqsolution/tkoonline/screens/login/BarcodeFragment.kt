bd23d5cf7724b61043bfce640f70c1465c85ec48
vladislav.k <vladislav.k@appstockus.com>
2019-09-16 10:49:13 +0300

    init

--- app/src/main/java/ru/iqsolution/tkoonline/screens/login/BarcodeFragment.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/screens/login/BarcodeFragment.kt
@@ -3,13 +3,12 @@ package ru.iqsolution.tkoonline.screens.login
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
+import android.widget.FrameLayout
 import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.Detector
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
-import org.jetbrains.anko.UI
-import org.jetbrains.anko.matchParent
-import org.jetbrains.anko.surfaceView
+import org.jetbrains.anko.*
 import ru.iqsolution.tkoonline.DANGER_PERMISSIONS
 import ru.iqsolution.tkoonline.extensions.areGranted
 import ru.iqsolution.tkoonline.screens.BaseFragment
@@ -28,7 +27,6 @@ class BarcodeFragment : BaseFragment() {
     private var isActive = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        //val width = appContext?.resources?.getDimensionPixelSize(R.dimen.barcode_width) ?: 1024
         barcodeDetector = BarcodeDetector.Builder(appContext)
             .setBarcodeFormats(Barcode.ALL_FORMATS)
             .build()
@@ -47,23 +45,28 @@ class BarcodeFragment : BaseFragment() {
             override fun release() {}
         })
         cameraSource = CameraSource.Builder(appContext, barcodeDetector)
+            .setRequestedPreviewSize(640, 480) // 4:3
             .setAutoFocusEnabled(true)
             .build()
         return UI {
-            cameraView = surfaceView {
+            frameLayout {
                 layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
-                holder.addCallback(object : SurfaceHolder.Callback {
+                cameraView = surfaceView {
+                    holder.addCallback(object : SurfaceHolder.Callback {
 
-                    override fun surfaceCreated(holder: SurfaceHolder) {
-                        startPreview()
-                    }
+                        override fun surfaceCreated(holder: SurfaceHolder) {
+                            startPreview()
+                        }
 
-                    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
+                        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
 
-                    override fun surfaceDestroyed(holder: SurfaceHolder) {
-                        cameraSource.stop()
-                    }
-                })
+                        override fun surfaceDestroyed(holder: SurfaceHolder) {
+                            cameraSource.stop()
+                        }
+                    })
+                }.lparams {
+                    gravity = Gravity.CENTER
+                }
             }
         }.view
     }
@@ -83,7 +86,22 @@ class BarcodeFragment : BaseFragment() {
     private fun startPreview() {
         try {
             if (appContext?.areGranted(*DANGER_PERMISSIONS) == true) {
-                cameraSource.start(cameraView.holder)
+                cameraView.apply {
+                    Timber.d("height " + height)
+                    if (height <= 0) {
+                        cameraSource.start()
+                    } else {
+                        Timber.d("start(cameraView.holder)")
+                        cameraSource.start(holder)
+                    }
+                    Timber.d("previewSize " + cameraSource.previewSize)
+                    //holder.setFixedSize(500, 281)
+                    //
+                    layoutParams = FrameLayout.LayoutParams(dip(250) * 3 / 4, dip(250)).apply {
+                        gravity = Gravity.CENTER
+                    }
+                }
+                //1280x720
                 //cameraSource.previewSize
             }
         } catch (e: IOException) {
