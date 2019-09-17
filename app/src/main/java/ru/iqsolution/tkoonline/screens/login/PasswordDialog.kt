26caa3e6942305b82992fdc09b25a63516c76004
vladislav.k <vladislav.k@appstockus.com>
2019-09-17 21:20:12 +0300

    init

--- app/src/main/java/ru/iqsolution/tkoonline/screens/login/PasswordDialog.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/screens/login/PasswordDialog.kt
@@ -1,8 +1,5 @@
-@file:Suppress("DEPRECATION")
-
 package ru.iqsolution.tkoonline.screens.login
 
-import android.app.DialogFragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +8,12 @@ import kotlinx.android.synthetic.main.dialog_password.*
 import org.jetbrains.anko.inputMethodManager
 import org.jetbrains.anko.sdk23.listeners.onClick
 import ru.iqsolution.tkoonline.R
-import ru.iqsolution.tkoonline.data.local.AESCBCPKCS7
 import ru.iqsolution.tkoonline.data.local.Preferences
 import ru.iqsolution.tkoonline.extensions.setMaxLength
 import ru.iqsolution.tkoonline.extensions.setOnlyNumbers
-import kotlin.math.absoluteValue
+import ru.iqsolution.tkoonline.screens.BaseDialogFragment
 
-class PasswordDialog : DialogFragment() {
+class PasswordDialog : BaseDialogFragment() {
 
     private var attemptsCount = 0
 
@@ -27,46 +23,44 @@ class PasswordDialog : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val preferences = Preferences(context)
-        val password = preferences.lockPassword?.let {
-            AESCBCPKCS7.decrypt(it)
-        }
-        var blockTime = preferences.nextAttemptsAfter.absoluteValue
+        val password = preferences.lockPassword
+        var blockTime = preferences.nextAttemptsAfter
         dialog_password.apply {
             setOnlyNumbers()
             setMaxLength(4)
         }
-        if (System.currentTimeMillis() - blockTime < WAIT_TIME) {
-            dialog_error.text = "Попробуйте еще раз позже"
+        if (isBlocking(blockTime)) {
+            disableInput()
+            dialog_error.text = "Попробуйте позже"
         }
         dialog_accept.onClick {
             dialog_error.text = ""
             val input = dialog_password.text.toString()
-            if (input.length == 4) {
+            if (isBlocking(blockTime)) {
+                disableInput()
+                dialog_error.text = "Попробуйте еще раз позже"
+            } else if (checkPassword(input)) {
                 when (password) {
                     null -> {
-                        preferences.lockPassword = AESCBCPKCS7.encrypt(input)
+                        preferences.lockPassword = input
                         onPrompted()
                     }
                     input -> onPrompted()
                     else -> {
-                        dialog_error.text = if (System.currentTimeMillis() - blockTime >= WAIT_TIME) {
-                            attemptsCount++
-                            if (attemptsCount > MAX_ATTEMPTS) {
-                                attemptsCount = 0
-                                blockTime = System.currentTimeMillis()
-                                preferences.nextAttemptsAfter = blockTime
-                                "Попробуйте еще раз позже"
-                            } else {
-                                val count = MAX_ATTEMPTS - attemptsCount
-                                resources.getQuantityString(R.plurals.attempts, count, count)
-                            }
+                        attemptsCount++
+                        if (attemptsCount > MAX_ATTEMPTS) {
+                            attemptsCount = 0
+                            blockTime = System.currentTimeMillis()
+                            preferences.nextAttemptsAfter = blockTime
+                            disableInput()
+                            dialog_error.text = "Попробуйте еще раз позже"
                         } else {
-                            "Попробуйте еще раз позже"
+                            val count = MAX_ATTEMPTS - attemptsCount
+                            dialog_password.isEnabled = true
+                            dialog_error.text = resources.getQuantityString(R.plurals.attempts, count, count)
                         }
                     }
                 }
-            } else {
-                dialog_error.text = "Короткий пароль"
             }
         }
         dialog_close.onClick {
@@ -75,9 +69,21 @@ class PasswordDialog : DialogFragment() {
         }
     }
 
-    override fun dismiss() {
-        context.inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
-        super.dismiss()
+    private fun checkPassword(input: CharSequence): Boolean {
+        if (input.length != 4) {
+            dialog_error.text = "Введите 4 цифры"
+            return false
+        }
+        return true
+    }
+
+    private fun isBlocking(blockTime: Long) = System.currentTimeMillis() - blockTime < WAIT_TIME
+
+    private fun disableInput() {
+        dialog_password.apply {
+            setText("")
+            isEnabled = false
+        }
     }
 
     private fun onPrompted() {
