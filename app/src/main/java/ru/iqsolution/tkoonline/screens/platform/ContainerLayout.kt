3fb16d5dcce0636bbe3e05f179d84b9508fe7113
vladislav.k <vladislav.k@appstockus.com>
2019-10-02 20:34:41 +0300

    init

--- app/src/main/java/ru/iqsolution/tkoonline/screens/platform/ContainerLayout.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/screens/platform/ContainerLayout.kt
@@ -3,11 +3,11 @@ package ru.iqsolution.tkoonline.screens.platform
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Context
-import android.graphics.Typeface
 import android.os.Build
 import android.text.Spannable
 import android.text.SpannableStringBuilder
-import android.text.style.StyleSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
@@ -16,11 +16,16 @@ import kotlinx.android.synthetic.main.merge_container.view.*
 import org.jetbrains.anko.sdk23.listeners.onClick
 import ru.iqsolution.tkoonline.R
 import ru.iqsolution.tkoonline.extensions.use
+import ru.iqsolution.tkoonline.models.Container
 import ru.iqsolution.tkoonline.models.ContainerType
 
-class ContainerLayout : LinearLayout {
+class ContainerLayout : LinearLayout, Container {
 
-    private var containerType = ContainerType.UNKNOWN
+    override var containerType = ContainerType.UNKNOWN.id
+
+    override var containerVolume = 0.0f
+
+    override var containerCount = 0
 
     @JvmOverloads
     constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : super(
@@ -45,21 +50,34 @@ class ContainerLayout : LinearLayout {
     init {
         View.inflate(context, R.layout.merge_container, this)
         arrow_up_volume.onClick {
-
+            if (containerVolume < 9.9f) {
+                containerVolume += 0.1f
+                volume_value.setRichText(context.getString(R.string.platform_volume, containerVolume))
+            }
         }
         arrow_down_volume.onClick {
-
+            if (containerVolume >= 0.1f) {
+                containerVolume -= 0.1f
+                volume_value.setRichText(context.getString(R.string.platform_volume, containerVolume))
+            }
         }
         arrow_up_count.onClick {
-
+            if (containerCount < 99) {
+                containerCount++
+                count_value.setRichText(context.getString(R.string.platform_count, containerCount))
+            }
         }
         arrow_down_count.onClick {
-
+            if (containerCount > 0) {
+                containerCount--
+                count_value.setRichText(context.getString(R.string.platform_count, containerCount))
+            }
         }
     }
 
     @SuppressLint("Recycle")
     private fun init(attrs: AttributeSet?) {
+        val type = ContainerType.UNKNOWN
         attrs?.let {
             context.obtainStyledAttributes(it, R.styleable.ContainerLayout).use { a ->
                 containerType = ContainerType.fromId(a.getString(R.styleable.ContainerLayout_containerType))
@@ -67,17 +85,29 @@ class ContainerLayout : LinearLayout {
         }
         icon_type.setImageResource(containerType.icon)
         text_type.text = containerType.shortName
+        if (type == ContainerType.BULK1 || type == ContainerType.BULK2) {
+            arrow_up_volume.visibility = INVISIBLE
+            arrow_down_volume.visibility = INVISIBLE
+            arrow_up_count.visibility = INVISIBLE
+            arrow_down_count.visibility = INVISIBLE
+            count_value.visibility = INVISIBLE
+        }
+    }
+
+    override fun addContainer(container: Container?) {
+        super.addContainer(container)
+        volume_value.setRichText(context.getString(R.string.platform_volume, containerVolume))
+        count_value.setRichText(context.getString(R.string.platform_count, containerCount))
     }
 
     override fun hasOverlappingRendering() = false
 
-    fun TextView.setTextBoldSpan(text: CharSequence, indices: List<Int>) {
-        require(indices.size % 2 == 0) { "The size of list must be an even number" }
-        val boldStyle = StyleSpan(Typeface.BOLD)
+    private fun TextView.setRichText(text: CharSequence) {
+        val smallStyle = RelativeSizeSpan(0.8f)
+        val colorStyle = ForegroundColorSpan(sd)
         setText(SpannableStringBuilder(text).apply {
-            indices.chunked(2).forEach {
-                setSpan(boldStyle, it[0], it[1], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-            }
+            setSpan(colorStyle, 0, 0, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            setSpan(smallStyle, 0, 0, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         })
     }
 }
