60a70c603c813ceb425a6c5d2fb208793dd95b8b
vladislav.k <vladislav.k@appstockus.com>
2019-10-06 15:53:24 +0300

    init

--- app/src/main/java/ru/iqsolution/tkoonline/screens/platforms/WaitDialog.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/screens/platforms/WaitDialog.kt
@@ -3,14 +3,24 @@ package ru.iqsolution.tkoonline.screens.platforms
 import android.app.Activity
 import android.os.Bundle
 import android.view.Window
+import kotlinx.android.synthetic.main.dialog_wait.*
 import ru.iqsolution.tkoonline.R
 import ru.iqsolution.tkoonline.screens.base.BaseDialog
 
 class WaitDialog(activity: Activity) : BaseDialog(activity) {
 
+    init {
+        setCancelable(false)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.dialog_wait)
+        dialog_cancel.setOnClickListener {
+            makeCallback<WaitListener> {
+                cancelWork()
+            }
+        }
     }
 }
--- app/src/main/java/ru/iqsolution/tkoonline/screens/platforms/WaitListener.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/screens/platforms/WaitListener.kt
@@ -1,16 +1,6 @@
 package ru.iqsolution.tkoonline.screens.platforms
 
-import ru.iqsolution.tkoonline.local.Preferences
-
 interface WaitListener {
 
-    fun openDialog(preferences: Preferences)
-
-    fun openSettingsDialog()
-
-    fun openPasswordDialog()
-
-    fun enterKioskMode()
-
-    fun exitKioskMode()
+    fun cancelWork()
 }
