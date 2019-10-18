857ee1ba171a6dceb902d4d17bc4d0ddf6d6470e
vladislav.k <vladislav.k@appstockus.com>
2019-10-18 13:53:51 +0300

    init

--- app/src/main/java/ru/iqsolution/tkoonline/models/PlatformStatus.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/models/PlatformStatus.kt
@@ -13,7 +13,7 @@ enum class PlatformStatus(
     CLEANED_TIMEOUT(11, R.color.colorStatusGreen, R.drawable.oval_green),
     PENDING(20, R.color.colorStatusYellow, R.drawable.oval_yellow),
     NOT_VISITED(30, R.color.colorStatusRed, R.drawable.oval_red),
-    NOT_CLEANED(31, R.color.colorStatusRed, R.drawable.oval_red);
+    NOT_CLEANED(31, R.color.colorStatusOrange, R.drawable.oval_orange);
 
     companion object {
 
