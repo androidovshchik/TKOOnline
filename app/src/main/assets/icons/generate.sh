51c04ef101c17da2ebe40b8a11734986fd6d4d32
vlad <vladkalyuzhnyu@gmail.com>
2019-10-31 08:09:32 +0300

    changes

--- app/src/main/assets/icons/generate.sh
+++ app/src/main/assets/icons/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for image in *.png; do 
-    for i in $(seq 0 5 175); do 
-        convert $image -virtual-pixel transparent -distort ScaleRotateTranslate 45 $image.$i.png;
-    done
+    #for i in $(seq 0 5 130); do 
+        convert $image -virtual-pixel transparent -distort ScaleRotateTranslate -45 $image;
+    #done
 done
