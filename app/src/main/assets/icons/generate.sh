f13c9aec922e146f9e80baa61766e8fb6648b94e
vlad <vladkalyuzhnyu@gmail.com>
2019-10-31 10:08:49 +0300

    changes

--- app/src/main/assets/icons/generate.sh
+++ app/src/main/assets/icons/generate.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 rm -rf gen
 mkdir -p gen
-for image in *.png; do 
-    # convert $image -virtual-pixel transparent -distort SRT -45 $image;
+cp *NaN.png gen/
+for image in *.png; do
+    if [[ $image == *NaN* ]]; then
+        continue;
+    fi
+    # convert $image -virtual-pixel transparent -distort SRT -45 $image
     # continue;
-    for i in $(seq -175 5 180); do 
+    for i in $(seq -180 5 175); do
         if [ $i -eq 0 ]; then
-            cp $image gen/${image%.*}0.png;
-            cp $image gen/${image%.*}360.png;
+            cp $image gen/${image%.*}0.png
+            cp $image gen/${image%.*}360.png
             continue;
         fi
         if [ $i -lt 0 ]; then
@@ -15,7 +19,7 @@ for image in *.png; do
         else
             filename=${image%.*}$i.png;
         fi
-        convert $image -virtual-pixel transparent -distort SRT $i gen/$filename;
+        convert $image -virtual-pixel transparent -distort SRT $i gen/$filename
     done
 done
 pngquant --quality=70 --ext .png -f gen/*
