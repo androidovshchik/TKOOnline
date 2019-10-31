1e2b9be4d479c7b357609a3ceec7f5dc7fac64c6
vlad <vladkalyuzhnyu@gmail.com>
2019-10-31 09:05:52 +0300

    changes

--- app/src/main/assets/icons/generate.sh
+++ app/src/main/assets/icons/generate.sh
@@ -4,7 +4,7 @@ mkdir -p gen
 for image in *.png; do 
     # convert $image -virtual-pixel transparent -distort SRT -45 $image;
     # continue;
-    for i in $(seq -175 5 180); do 
+    for i in $(seq -180 5 180); do 
         if [ $i -eq 0 ]; then
             convert $image gen/${image%.*}0.png;
             continue;
@@ -17,4 +17,4 @@ for image in *.png; do
         convert $image -virtual-pixel transparent -distort SRT $i gen/$filename;
     done
 done
-pngquant --quality=70 gen/*
+pngquant --quality=70 --ext .png -f gen/*
