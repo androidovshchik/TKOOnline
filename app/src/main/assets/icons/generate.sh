6cf4f0ffc5a3b54555353bf04feb43f2bdc1f528
vlad <vladkalyuzhnyu@gmail.com>
2019-10-31 09:08:49 +0300

    changes

--- app/src/main/assets/icons/generate.sh
+++ app/src/main/assets/icons/generate.sh
@@ -4,9 +4,10 @@ mkdir -p gen
 for image in *.png; do 
     # convert $image -virtual-pixel transparent -distort SRT -45 $image;
     # continue;
-    for i in $(seq -180 5 180); do 
+    for i in $(seq -175 5 180); do 
         if [ $i -eq 0 ]; then
-            convert $image gen/${image%.*}0.png;
+            cp $image gen/${image%.*}0.png;
+            cp $image gen/${image%.*}360.png;
             continue;
         fi
         if [ $i -lt 0 ]; then
