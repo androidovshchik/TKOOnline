e726f2719b78bceeac1038c8125b3fdfe7614677
vlad <vladkalyuzhnyu@gmail.com>
2019-10-31 08:22:04 +0300

    changes

--- app/src/main/assets/icons/generate.sh
+++ app/src/main/assets/icons/generate.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 for image in *.png; do 
-    #for i in $(seq 0 5 130); do 
-        convert $image -virtual-pixel transparent -distort ScaleRotateTranslate -45 $image;
-    #done
+    for i in $(seq -175 5 175); do 
+        if [ $i -eq 0 ]; then
+            continue;
+        fi
+        if [ $i -lt 0 ]; then
+            filename=${image%.*}.$((360 + i)).png;
+        else
+            filename=${image%.*}.$i.png;
+        fi
+        convert $image -virtual-pixel transparent -distort ScaleRotateTranslate $i $filename;
+    done
 done
