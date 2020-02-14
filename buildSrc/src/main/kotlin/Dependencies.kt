688ec6e0ae26e51d1dfe689f80f06cab7ead342b
vlad <vladkalyuzhnyu@gmail.com>
2020-02-14 21:27:18 +0300

    gradle

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,10 +4,10 @@
 @file:Suppress("unused")
 
 object Versions {
-    const val kotlin = "1.3.50"
-    const val kodein = "6.3.3"
+    const val kotlin = "1.3.61"
+    const val kodein = "6.5.0"
     const val anko = "0.10.8"
-    const val room = "2.2.1"
+    const val room = "2.2.3"
     const val lifecycle = "2.1.0"
     const val retrofit = "2.6.0"
     const val andresguard = "1.2.17"
