b508a797c138aa93270d5adc2f6ab4fd58e7be7c
vlad <vladkalyuzhnyu@gmail.com>
2020-02-16 14:03:29 +0300

    fix 404

--- app/src/main/java/ru/iqsolution/tkoonline/screens/login/LoginPresenter.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/screens/login/LoginPresenter.kt
@@ -8,6 +8,7 @@ import com.google.gson.JsonSyntaxException
 import kotlinx.coroutines.*
 import org.joda.time.DateTime
 import org.kodein.di.generic.instance
+import retrofit2.HttpException
 import ru.iqsolution.tkoonline.PATTERN_DATETIME
 import ru.iqsolution.tkoonline.local.FileManager
 import ru.iqsolution.tkoonline.local.entities.AccessToken
@@ -57,30 +58,39 @@ class LoginPresenter(context: Context) : BasePresenter<LoginContract.View>(conte
         qrCodeJson = data
         baseJob.cancelChildren()
         launch {
-            val responseAuth = server.login(
-                qrCode.carId.toString(),
-                qrCode.pass,
-                preferences.lockPassword?.toInt()
-            )
-            preferences.bulk {
-                accessToken = responseAuth.accessKey
-                expiresWhen = responseAuth.expireTime
-                allowPhotoRefKp = responseAuth.noKpPhoto == 1
-                serverTime = responseAuth.currentTime.toString(PATTERN_DATETIME)
-                elapsedTime = SystemClock.elapsedRealtime()
-                vehicleNumber = qrCode.regNum
-                queName = responseAuth.queName
-                carId = qrCode.carId
-                tokenId = withContext(Dispatchers.IO) {
-                    db.tokenDao().insert(AccessToken().apply {
-                        token = responseAuth.accessKey
-                        queName = responseAuth.queName
-                        carId = qrCode.carId
-                        expires = DateTime.parse(responseAuth.expireTime, PATTERN_DATETIME)
-                    })
+            try {
+                val responseAuth = server.login(
+                    qrCode.carId.toString(),
+                    qrCode.pass,
+                    preferences.lockPassword?.toInt()
+                )
+                preferences.bulk {
+                    accessToken = responseAuth.accessKey
+                    expiresWhen = responseAuth.expireTime
+                    allowPhotoRefKp = responseAuth.noKpPhoto == 1
+                    serverTime = responseAuth.currentTime.toString(PATTERN_DATETIME)
+                    elapsedTime = SystemClock.elapsedRealtime()
+                    vehicleNumber = qrCode.regNum
+                    queName = responseAuth.queName
+                    carId = qrCode.carId
+                    tokenId = withContext(Dispatchers.IO) {
+                        db.tokenDao().insert(AccessToken().apply {
+                            token = responseAuth.accessKey
+                            queName = responseAuth.queName
+                            carId = qrCode.carId
+                            expires = DateTime.parse(responseAuth.expireTime, PATTERN_DATETIME)
+                        })
+                    }
+                }
+                reference.get()?.onLoggedIn()
+            } catch (e: HttpException) {
+                if (e.code() == 404) {
+                    Timber.e(e)
+                    reference.get()?.showError("Сервер не отвечает, проверьте настройки соединения")
+                } else {
+                    throw e
                 }
             }
-            reference.get()?.onLoggedIn()
         }
     }
 
