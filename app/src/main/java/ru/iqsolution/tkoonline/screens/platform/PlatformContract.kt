08fb154118617b1fcfe58b24459f82d40a37d283
vlad <vladkalyuzhnyu@gmail.com>
2020-02-17 16:03:18 +0300

    platform

--- app/src/main/java/ru/iqsolution/tkoonline/screens/platform/PlatformContract.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/screens/platform/PlatformContract.kt
@@ -3,6 +3,7 @@ package ru.iqsolution.tkoonline.screens.platform
 import ru.iqsolution.tkoonline.local.entities.CleanEventRelated
 import ru.iqsolution.tkoonline.local.entities.PhotoEvent
 import ru.iqsolution.tkoonline.local.entities.Platform
+import ru.iqsolution.tkoonline.models.PlatformContainers
 import ru.iqsolution.tkoonline.screens.base.IBasePresenter
 import ru.iqsolution.tkoonline.screens.base.IBaseView
 
@@ -16,12 +17,12 @@ interface PlatformContract {
 
         fun loadPhotoEvents(kpId: Int)
 
-        fun savePlatformEvents(platforms: List<Platform>)
+        fun savePlatformEvents(platform: PlatformContainers, platforms: List<Platform>)
     }
 
     interface View : IBaseView, ConfirmListener, GalleryListener {
 
-        fun onLinkedPlatforms(event: List<Platform>)
+        fun onLinkedPlatforms(event: List<Platform>?)
 
         fun onCleanEvents(event: CleanEventRelated?)
 
