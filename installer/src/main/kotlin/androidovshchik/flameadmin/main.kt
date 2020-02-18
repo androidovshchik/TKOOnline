d449c0c7196231d1fa03ade573f15e8232265a0c
vlad <vladkalyuzhnyu@gmail.com>
2020-02-18 12:12:31 +0300

    installer

--- installer/src/main/kotlin/androidovshchik/flameadmin/main.kt
+++ installer/src/main/kotlin/androidovshchik/flameadmin/main.kt
@@ -1,11 +1,17 @@
 package androidovshchik.flameadmin
 
 import org.js.neutralino.Neutralino
+import org.w3c.dom.HTMLButtonElement
+import kotlin.browser.document
+import kotlin.browser.window
 
 fun main() {
-    Neutralino.app.exit({
-        it.message
-    }, {
-
+    val button = document.getElementById("install") as HTMLButtonElement
+    button.addEventListener("click", {
+        Neutralino.os.runCommand("adb", {
+            window.alert(it.stdout)
+        }, {
+            window.alert(it?.toString() ?: "error")
+        })
     })
 }
