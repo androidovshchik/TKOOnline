b18cf9915ad9118bbb611e361ef72a3ab5de39ee
vlad <vladkalyuzhnyu@gmail.com>
2020-02-19 09:47:35 +0300

    installer main

--- installer/src/main/kotlin/ru/iqsolution/tkonline/main.kt
+++ installer/src/main/kotlin/ru/iqsolution/tkonline/main.kt
@@ -10,6 +10,10 @@ import org.w3c.dom.HTMLButtonElement
 import kotlin.browser.document
 import kotlin.browser.window
 
+var waitDialog: dynamic = null
+
+var promptMessage: String? = null
+
 fun main() {
     window.onerror = { message, _, _, _, _ ->
         Neutralino.debug.log(LogType.ERROR, message, {}, {})
@@ -17,26 +21,21 @@ fun main() {
     bootbox.setLocale("ru")
     val button = document.getElementById("install") as HTMLButtonElement
     button.addEventListener("click", {
-        val dialog = bootbox.dialog(BootboxWaitDialog());
-        /*bootbox.confirm("fddfsdf") {
-
-        } as Unit*/
-// do something in the background
-        //dialog.modal("hide");
-        Neutralino.filesystem.readDirectory(".", {
-            if (it != null) {
-                it.files.forEach { file ->
-                    if (file.type == FileType.FILE) {
-                        if (file.name.endsWith(".apk")) {
-                            bootbox.alert(BootboxAlert("success: ${file.name}"))
-                        }
-                    }
-                }
-            } else {
-                bootbox.alert(BootboxAlert("error: ${it.toString()}"))
+        waitDialog = bootbox.dialog(BootboxWaitDialog())
+        waitDialog?.on("hidden.bs.modal") {
+            promptMessage?.let {
+                bootbox.alert(BootboxAlert(it))
             }
+        }
+        Neutralino.filesystem.readDirectory(".", { data ->
+            val apkFilename = data?.files?.firstOrNull { it.type == FileType.FILE && it.name.endsWith(".apk") }?.name
+            if (apkFilename == null) {
+                showPrompt("Не найден apk файл")
+                return@readDirectory
+            }
+            showPrompt("success: $apkFilename")
         }, {
-            bootbox.alert(BootboxAlert("error: ${it?.toString()}"))
+            showPrompt()
         })
         /*Neutralino.os.runCommand("app/tools/adb-linux install -r -t app/assets/tkoonline-release.apk", {
             bootbox.confirm(BootboxConfirm("success: ${it.stdout}"))
@@ -44,4 +43,11 @@ fun main() {
             bootbox.confirm(BootboxConfirm("error: ${it?.toString()}"))
         })*/
     })
+}
+
+private fun showPrompt(message: String = "Неизвестная ошибка") {
+    promptMessage = message
+    window.setTimeout({
+        waitDialog?.modal("hide")
+    }, 500)
 }
