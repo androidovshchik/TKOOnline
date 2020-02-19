7fc2109583fca9d68d55729cd1ad9c688c6f8679
vlad <vladkalyuzhnyu@gmail.com>
2020-02-19 06:26:12 +0300

    bootbox

--- installer/webpack.config.d/config.js
+++ installer/webpack.config.d/config.js
@@ -3,8 +3,19 @@ const path = require('path');
 config.output.path = path.join(__dirname, '/../tkonline/app/assets');
 config.output.filename = 'app.min.js';
 
+const concat = require('concat');
+
+concat([
+    'node_modules/bootstrap/dist/css/bootstrap.min.css'
+], path.join(__dirname, '/../tkonline/app/assets/vendor.min.css'));
+concat([
+    'node_modules/jquery/dist/jquery.slim.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+    'node_modules/bootbox/dist/bootbox.all.min.js'
+], path.join(__dirname, '/../tkonline/app/assets/vendor.min.js'));
+
 if (defined.PRODUCTION) {
-    const TerserPlugin = require('./node_modules/terser-webpack-plugin');
+    const TerserPlugin = require('terser-webpack-plugin');
 
     config.optimization = {
         minimize: true,
