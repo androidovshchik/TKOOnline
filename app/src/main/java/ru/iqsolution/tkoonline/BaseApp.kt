aef5ac65dd5aa2c10aa92338ed84b3058dbb424e
vlad <vladkalyuzhnyu@gmail.com>
2020-02-20 09:01:15 +0300

    flavors

--- app/src/main/java/ru/iqsolution/tkoonline/BaseApp.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/BaseApp.kt
@@ -9,12 +9,6 @@ import androidx.camera.camera2.Camera2Config
 import androidx.camera.core.CameraXConfig
 import coil.Coil
 import coil.ImageLoader
-import com.elvishew.xlog.LogConfiguration
-import com.elvishew.xlog.XLog
-import com.elvishew.xlog.flattener.PatternFlattener
-import com.elvishew.xlog.printer.file.FilePrinter
-import com.elvishew.xlog.printer.file.backup.NeverBackupStrategy
-import com.elvishew.xlog.printer.file.naming.DateFileNameGenerator
 import io.github.inflationx.calligraphy3.CalligraphyConfig
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor
 import io.github.inflationx.viewpump.ViewPump
@@ -25,18 +19,14 @@ import org.joda.time.DateTimeZone
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.bind
-import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 import ru.iqsolution.tkoonline.extensions.isOreoPlus
-import ru.iqsolution.tkoonline.local.Preferences
 import ru.iqsolution.tkoonline.local.localModule
 import ru.iqsolution.tkoonline.remote.remoteModule
 import ru.iqsolution.tkoonline.services.workers.MidnightWorker
-import timber.log.Timber
-import java.io.File
 
 @Suppress("unused")
-class MainApp : Application(), KodeinAware, CameraXConfig.Provider {
+abstract class BaseApp : Application(), KodeinAware, CameraXConfig.Provider {
 
     override val kodein by Kodein.lazy {
 
@@ -49,33 +39,10 @@ class MainApp : Application(), KodeinAware, CameraXConfig.Provider {
         import(remoteModule)
     }
 
-    val preferences: Preferences by instance()
-
     override fun getCameraXConfig() = Camera2Config.defaultConfig()
 
     override fun onCreate() {
         super.onCreate()
-        getExternalFilesDir(null)?.let {
-            val folder = File(it, "logs").apply {
-                mkdirs()
-            }
-            val config = LogConfiguration.Builder()
-                .t()
-                .build()
-            val filePrinter = FilePrinter.Builder(folder.path)
-                .fileNameGenerator(DateFileNameGenerator())
-                .backupStrategy(NeverBackupStrategy())
-                .flattener(PatternFlattener("{d yyyy-MM-dd HH:mm:ss.SSS} {l}: {m}"))
-                .build()
-            XLog.init(config, filePrinter)
-        }
-        Timber.plant(LogTree(preferences.enableLogs))
-        if (BuildConfig.DEBUG) {
-            Class.forName("com.facebook.stetho.Stetho")
-                .getDeclaredMethod("initializeWithDefaults", Context::class.java)
-                .invoke(null, applicationContext)
-        }
-        DateTimeZone.setProvider(ResourceZoneInfoProvider(applicationContext))
         if (isOreoPlus()) {
             notificationManager.createNotificationChannel(
                 NotificationChannel(CHANNEL_DEFAULT, CHANNEL_DEFAULT, NotificationManager.IMPORTANCE_LOW).also {
@@ -83,6 +50,7 @@ class MainApp : Application(), KodeinAware, CameraXConfig.Provider {
                 }
             )
         }
+        DateTimeZone.setProvider(ResourceZoneInfoProvider(applicationContext))
         MidnightWorker.launch(applicationContext)
         Coil.setDefaultImageLoader(ImageLoader(applicationContext) {
             availableMemoryPercentage(0.5)
@@ -105,8 +73,5 @@ class MainApp : Application(), KodeinAware, CameraXConfig.Provider {
                 )
                 .build()
         )
-        /*FileManager(applicationContext).deleteAllFiles()
-        //preferences.clear()
-        deleteDatabase("app.db")*/
     }
 }
