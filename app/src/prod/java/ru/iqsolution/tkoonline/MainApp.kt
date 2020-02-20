a3d3eefa448090ef99ce78dd76f8c806d92802d9
vlad <vladkalyuzhnyu@gmail.com>
2020-02-20 09:42:11 +0300

    flavors

--- app/src/prod/java/ru/iqsolution/tkoonline/MainApp.kt
+++ app/src/prod/java/ru/iqsolution/tkoonline/MainApp.kt
@@ -1,46 +1,7 @@
 package ru.iqsolution.tkoonline
 
-import android.content.Context
-import com.elvishew.xlog.LogConfiguration
-import com.elvishew.xlog.XLog
-import com.elvishew.xlog.flattener.PatternFlattener
-import com.elvishew.xlog.printer.file.FilePrinter
-import com.elvishew.xlog.printer.file.backup.NeverBackupStrategy
-import com.elvishew.xlog.printer.file.naming.DateFileNameGenerator
-import org.kodein.di.generic.instance
-import ru.iqsolution.tkoonline.local.Preferences
-import timber.log.Timber
-import java.io.File
-
 @Suppress("unused")
 class MainApp : BaseApp() {
 
-    val preferences: Preferences by instance()
-
-    override fun onCreate() {
-        super.onCreate()
-        getExternalFilesDir(null)?.let {
-            val folder = File(it, "logs").apply {
-                mkdirs()
-            }
-            val config = LogConfiguration.Builder()
-                .t()
-                .build()
-            val filePrinter = FilePrinter.Builder(folder.path)
-                .fileNameGenerator(DateFileNameGenerator())
-                .backupStrategy(NeverBackupStrategy())
-                .flattener(PatternFlattener("{d yyyy-MM-dd HH:mm:ss.SSS} {l}: {m}"))
-                .build()
-            XLog.init(config, filePrinter)
-        }
-        Timber.plant(LogTree(preferences.enableLogs))
-        if (BuildConfig.DEBUG) {
-            Class.forName("com.facebook.stetho.Stetho")
-                .getDeclaredMethod("initializeWithDefaults", Context::class.java)
-                .invoke(null, applicationContext)
-        }
-        /*FileManager(applicationContext).deleteAllFiles()
-        //preferences.clear()
-        deleteDatabase("app.db")*/
-    }
+    override fun init() {}
 }
