031c00356de48a42b883af263f3f9bc63bf56f04
vlad <vladkalyuzhnyu@gmail.com>
2020-02-24 16:03:51 +0300

    telemetry

--- app/src/main/java/ru/iqsolution/tkoonline/services/BaseService.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/services/BaseService.kt
@@ -1,10 +1,13 @@
 package ru.iqsolution.tkoonline.services
 
+import android.annotation.SuppressLint
 import android.app.Service
+import android.os.PowerManager
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import org.jetbrains.anko.powerManager
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import timber.log.Timber
@@ -15,6 +18,25 @@ abstract class BaseService : Service(), KodeinAware, CoroutineScope {
     override val kodein by kodein()
 
     protected val serviceJob = SupervisorJob()
+
+    protected var wakeLock: PowerManager.WakeLock? = null
+
+    @SuppressLint("WakelockTimeout")
+    protected fun acquireWakeLock(name: String) {
+        // todo java class
+        if (wakeLock == null) {
+            wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, name).apply {
+                acquire()
+            }
+        }
+    }
+
+    protected fun releaseWakeLock() {
+        wakeLock?.let {
+            it.release()
+            wakeLock = null
+        }
+    }
 
     @Suppress("RedundantOverride")
     override fun onDestroy() {
--- app/src/main/java/ru/iqsolution/tkoonline/services/telemetry/PreferenceHolder.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/services/telemetry/PreferenceHolder.kt
@@ -1,5 +1,9 @@
 package ru.iqsolution.tkoonline.services.telemetry
 
+import androidx.annotation.UiThread
+import com.chibatching.kotpref.bulk
+import ru.iqsolution.tkoonline.local.Preferences
+
 class PreferenceHolder {
 
     @Volatile
@@ -11,5 +15,21 @@ class PreferenceHolder {
     @Volatile
     private var packageId = 0
 
+    @UiThread
+    fun init(preferences: Preferences) {
+        tokenId = preferences.tokenId
+        mileage = preferences.mileage
+        packageId = preferences.packageId
+    }
 
+    @UiThread
+    fun save(preferences: Preferences) {
+        let {
+            preferences.bulk {
+                tokenId = it.tokenId
+                mileage = it.mileage
+                packageId = it.packageId
+            }
+        }
+    }
 }
