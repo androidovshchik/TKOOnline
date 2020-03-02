2d907d6cf6240d5fb580fc4976803dfe657d1d04
vlad <vladkalyuzhnyu@gmail.com>
2020-03-02 12:31:53 +0300

    TelemetryConfig

--- app/src/main/java/ru/iqsolution/tkoonline/models/TelemetryConfig.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/models/TelemetryConfig.kt
@@ -7,41 +7,40 @@ open class TelemetryConfig {
 
     /**
      * Событие пройдена дистанция
-     * Данное событие генерируется только в состоянии движения при перемещении автомобиля от базовой точки на расстояние больше 200 метров.
+     * Данное событие генерируется только в состоянии движения при перемещении автомобиля от базовой точки
      */
     @SerializedName("base_distance.meters")
-    val baseDistance = 200
+    val baseDistance = 120
 
-    // Направление движения отклоняется от базового на величину 5 градусов
+    // Направление движения отклоняется от базового
     @SerializedName("base_degree.degrees")
     val baseDegree = 5
 
-    // Скорость выше параметра минимальной скорости (10км/ч)
+    // Скорость выше параметра минимальной скорости
     @SerializedName("min_speed.km_h")
-    val minSpeed = 10
+    val minSpeed = 7
 
-    // минимальное время - 30 секунд
     @SerializedName("min_time.seconds")
     val minTime = 30
 
     /**
      * Событие стоянка
-     * Данное событие генерируется в состоянии остановка если данное состояние не изменено в течение 2 минут
+     * Данное событие генерируется в состоянии остановка если данное состояние не изменено
      */
     @SerializedName("parking_time.seconds")
-    private val _parkingTime = 2 * 60
+    private val _parkingTime = 60
 
     val parkingTime = _parkingTime * 1000L
 
-    // Для состояния стоянка - 5 минут
+    // Для состояния стоянка
     @SerializedName("parking_delay.seconds")
-    private val _parkingDelay = 5 * 60
+    private val _parkingDelay = 120
 
     val parkingDelay = _parkingDelay * 1000L
 
-    // Для состояния движения и остановка - 1 минута
+    // Для состояния движения и остановка
     @SerializedName("moving_delay.seconds")
-    private val _movingDelay = 60
+    private val _movingDelay = 10
 
     val movingDelay = _movingDelay * 1000L
 
@@ -57,7 +56,7 @@ open class TelemetryConfig {
      * Max timeout of no input locations
      */
     @SerializedName("location_max_delay.seconds")
-    private val _locationMaxDelay = 60
+    private val _locationMaxDelay = 30
 
     val locationMaxDelay = _locationMaxDelay * 1000L
 
