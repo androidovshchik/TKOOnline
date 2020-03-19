c9252148b381250eb4898e398d1ade5afd9f20b5
vlad <vladkalyuzhnyu@gmail.com>
2020-03-19 15:37:58 +0300

    dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,12 +4,12 @@
 @file:Suppress("unused")
 
 object Versions {
-    const val kotlin = "1.3.61"
-    const val kodein = "6.5.0"
+    const val kotlin = "1.3.70"
+    const val kodein = "6.5.3"
     const val anko = "0.10.8"
-    const val room = "2.2.4"
+    const val room = "2.2.5"
     const val lifecycle = "2.1.0"
-    const val retrofit = "2.6.0"
+    const val retrofit = "2.7.2"
     const val andresguard = "1.2.17"
     const val stetho = "1.5.1"
 }
