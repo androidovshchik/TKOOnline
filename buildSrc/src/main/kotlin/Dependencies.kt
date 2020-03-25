790631a1b235a7ea5f9ff7b7bc0edc001d04e5fc
vlad <vladkalyuzhnyu@gmail.com>
2020-03-25 09:52:27 +0300

    layout qr code

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,12 +4,12 @@
 @file:Suppress("unused")
 
 object Versions {
-    const val kotlin = "1.3.70"
-    const val kodein = "6.5.3"
+    const val kotlin = "1.3.71"
+    const val kodein = "6.5.4"
     const val anko = "0.10.8"
     const val room = "2.2.5"
     const val lifecycle = "2.1.0"
-    const val retrofit = "2.7.2"
+    const val retrofit = "2.8.0"
     const val andresguard = "1.2.17"
     const val stetho = "1.5.1"
 }
