c19da0ea2465e178cd6ced1f69ab13c6764e4ef1
vlad <vladkalyuzhnyu@gmail.com>
2021-09-28 18:18:04 +0300

    ContainerType def volume

--- app/src/main/java/ru/iqsolution/tkoonline/models/ContainerType.kt
+++ app/src/main/java/ru/iqsolution/tkoonline/models/ContainerType.kt
@@ -7,17 +7,18 @@ import ru.iqsolution.tkoonline.R
 enum class ContainerType(
     val id: String,
     val icon: Int,
+    val defVolume: Float,
     val isEditable: Boolean,
     val shortName: String = id
 ) {
     // UNKNOWN.id is only my own implementation
-    UNKNOWN("НЕИЗВЕСТНЫЙ", R.drawable.ic_question, false, "Неиз."),
-    REGULAR("ТБО", R.drawable.ic_trash_can, true),
-    BUNKER("КГМ", R.drawable.ic_trash_bin, true),
-    BULK1("БТ", R.drawable.ic_rubbish_bag, false),
-    BULK2("БЕСТАРНЫЙ", R.drawable.ic_rubbish_bag, false, "БТ"),
-    SPECIAL1("СПЕЦИАЛЬНЫЙ", R.drawable.ic_triangle, true, "Спец."),
-    SPECIAL2("СПЕЦИАЛИЗИРОВАННЫЙ", R.drawable.ic_triangle, true, "Спец.");
+    UNKNOWN("НЕИЗВЕСТНЫЙ", R.drawable.ic_question, 0f, false, "Неиз."),
+    REGULAR("ТБО", R.drawable.ic_trash_can, 0.8f, true),
+    BUNKER("КГМ", R.drawable.ic_trash_bin, 8f, true),
+    BULK1("БТ", R.drawable.ic_rubbish_bag, 1f, false),
+    BULK2("БЕСТАРНЫЙ", R.drawable.ic_rubbish_bag, 1f, false, "БТ"),
+    SPECIAL1("СПЕЦИАЛЬНЫЙ", R.drawable.ic_triangle, 1.1f, true, "Спец."),
+    SPECIAL2("СПЕЦИАЛИЗИРОВАННЫЙ", R.drawable.ic_triangle, 1.1f, true, "Спец.");
 
     companion object {
 
